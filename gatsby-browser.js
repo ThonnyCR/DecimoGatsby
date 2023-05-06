8668f915100cd2aeab5128998d14de9c4c2d62b3
Molina60217N <molinasalas16@gmail.com>
2023-05-06 17:30:12 -0600

    eliminacion de archivos inutiles

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ScrollProvider } from "./src/contexts/ScrollContext";
-
-//wrapRootElement is useful to set up any context providers that will wrap your application.
-export const wrapRootElement = ({ element }) => {
-  return <ScrollProvider>{element}</ScrollProvider>;
-};
